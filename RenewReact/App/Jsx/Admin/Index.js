6090974172bd262d692287062599d2ef1e65be77
gunnarsireus <gunnar.sireus@gmail.com>
2016-10-07 07:17:09 +0200

    Panels3 started

--- RenewReact/App/Jsx/Admin/Index.js
+++ RenewReact/App/Jsx/Admin/Index.js
@@ -1,193 +1,60 @@
 ﻿import React, { Component } from 'react';
 import style from '../../css/site.css';
+import Panels3 from '../Common/Panels3.js';
 export default class Index extends Component {
+    constructor(props) {
+        super(props);
+    };
     render() {
-        return  <div className="row">
-        <div className="col-md-2 site-page-control">
-            <h3 className="site-page-header">Profil</h3>
-            <div className="row">
-                <div className="col-md-12">
+        return      <div class="row">
+        <div class="col-md-2 site-page-control">
+            <h3 class="site-page-header">Profil</h3>
+            <div class="row">
+                <div class="col-md-12">
                     <h4>Användare</h4>
                     admin
                 </div>
             </div>
-            <div className="row site-row-spacing">
-                <div className="col-md-12">
+            <div class="row site-row-spacing">
+                <div class="col-md-12">
                     <h4>Förening</h4>
                     --
                 </div>
             </div>
-            <div className="row site-row-spacing">
-                <div className="col-md-12">
+            <div class="row site-row-spacing">
+                <div class="col-md-12">
                     <h4>Behörighet</h4>
                     Admin
                 </div>
             </div>
-            <div className="row site-row-spacing">
-                <div className="col-md-12">
+            <div class="row site-row-spacing">
+                <div class="col-md-12">
                     <h4>Inloggad sedan</h4>
                     2016-10-05 18:52
                 </div>
             </div>
         </div>
-        <div className="col-md-10 site-page-content">
-            <div className="row">
-                <div className="col-md-6">
-                    
-
-<div className="panel panel-default">
-<div className="panel-heading">
-<h3 className="panel-title" id="title">Uppdatera kontoinformation</h3>
-</div>
-<div className="panel-body">
-<form id="update-profile-form">
-<div className="form-group">
-<label className="control-label col-md-3" style={{marginLeft:"-15px",marginRight:"15px"}} htmlFor="firstname">Förnamn</label>
-<div className="col-md-9 input-group">
-<span className="input-group-addon">
-<span className="glyphicon glyphicon-user"></span>
-</span>
-<input className="form-control"
-data-autoajax="true"
-data-val="true"
-data-val-length-min="2"
-data-val-length-max="45"
-data-val-length="Förnamn måste vara mellan 2 och 45 tecken"
-data-val-required="Förnamn får inte vara tom"
-id="firstname"
-name="firstname"
-type="text"
-value="" />
-</div>
-</div>
-<div className="form-group">
-<label className="control-label col-md-3" style={{marginLeft:"-15px",marginRight:"15px"}} htmlFor="lastname">Efternamn</label>
-<div className="col-md-9 input-group">
-<span className="input-group-addon">
-<span className="glyphicon glyphicon-user"></span>
-</span>
-<input className="form-control"
-data-autoajax="true"
-data-val="true"
-data-val-length-min="2"
-data-val-length-max="45"
-data-val-length="Efternamn måste vara mellan 2 och 45 tecken"
-data-val-required="Efternamn får inte vara tom"
-id="lastname"
-name="lastname"
-type="text"
-value="" />
-</div>
-</div>
-<div className="form-group">
-<label className="control-label col-md-3" style={{marginLeft:"-15px",marginRight:"15px"}} htmlFor="phone">Telefon</label>
-<div className="col-md-9 input-group">
-<span className="input-group-addon">
-<span className="glyphicon glyphicon-phone-alt"></span>
-</span>
-<input className="form-control"
-data-autoajax="true"
-data-val="true"
-data-val-length-max="45"
-data-val-length="Telefon får inte överstiga 45 tecken"
-id="phone"
-name="phone"
-//onKeyPress = "return Site.Validation.isPhoneNumber(event)"
-type="text"
-value="" />
-</div>
-</div>
-<div className="row">
-<div className="col-md-6">
-</div>
-<div className="col-md-6">
-<button className="site-buttonmargin-left pull-right btn-info btn update-profile-form-buttons" data-action="8" type="button">Spara</button>
-</div>
-</div>
-</form></div></div>
+        <div class="col-md-10 site-page-content">
+            <div class="row">
+                <div class="col-md-6">
+                    <Panels3 text1={'Förnamn'} 
+                             text2={'Efternamn'}
+                             text3={'Telefon'}
+                             icon1={'glyphicon glyphicon-user'}
+                             icon2={'glyphicon glyphicon-user'}
+                             icon3={'glyphicon glyphicon-phone-alt'}/>
                 </div>
-                <div className="col-md-6">
-                    
-
-<div className="panel panel-default">
-<div className="panel-heading">
-<h3 className="panel-title" id="title">Byt l&#246;senord</h3>
-</div>
-<div className="panel-body">
-<form id="change-password-form">
-<div className="form-group">
-<label className="control-label col-md-3" style={{marginLeft:"-15px",marginRight:"15px"}} htmlFor="password">Gällande</label>
-<div className="col-md-9 input-group">
-<span className="input-group-addon">
-<span className="glyphicon glyphicon-lock"></span>
-</span>
-<input className="form-control"
-data-autoajax="true"
-data-val="true"
-data-val-length-min="6"
-data-val-length-max="45"
-data-val-length="Gällande måste vara mellan 6 och 45 tecken"
-data-val-required="Gällande får inte vara tom"
-id="password"
-name="password"
-type="password"
-value="" />
-</div>
-</div>
-<div className="form-group">
-<label className="control-label col-md-3" style={{marginLeft:"-15px",marginRight:"15px"}} htmlFor="newpassword1">Nytt</label>
-<div className="col-md-9 input-group">
-<span className="input-group-addon">
-<span className="glyphicon glyphicon-lock"></span>
-</span>
-<input className="form-control"
-data-autoajax="true"
-data-val="true"
-data-val-length-min="6"
-data-val-length-max="45"
-data-val-length="Nytt måste vara mellan 6 och 45 tecken"
-data-val-required="Nytt får inte vara tom"
-id="newpassword1"
-name="newpassword1"
-//onKeyUp    = "return Site.Validation.isSecurePassword(event)"
-type="password"
-value="" />
-</div>
-</div>
-<div className="form-group">
-<label className="control-label col-md-3" style={{marginLeft:"-15px",marginRight:"15px"}} htmlFor="newpassword2">Bekräfta</label>
-<div className="col-md-9 input-group">
-<span className="input-group-addon">
-<span className="glyphicon glyphicon-lock"></span>
-</span>
-<input className="form-control"
-data-autoajax="true"
-data-val="true"
-data-val-length-min="6"
-data-val-length-max="45"
-data-val-length="Bekräfta måste vara mellan 6 och 45 tecken"
-data-val-required="Bekräfta får inte vara tom"
-id="newpassword2"
-name="newpassword2"
-//onKeyUp    = "return Site.Validation.isSecurePassword(event)"
-type="password"
-value="" />
-</div>
-</div>
-<div className="row">
-<div className="col-md-6">
-</div>
-<div className="col-md-6">
-<button className="site-buttonmargin-left pull-right btn-info btn change-password-form-buttons" data-action="8" type="button">Spara</button>
-</div>
-</div>
-</form></div></div>
+                <div class="col-md-6">
+                    <Panels3 text1={'Gällande'} 
+                             text2={'Nytt'}
+                             text3={'Bekräfta'}
+                             icon1={'glyphicon glyphicon-lock'}
+                             icon2={'glyphicon glyphicon-lock'}
+                             icon3={'glyphicon glyphicon-lock'}/>
                 </div>
             </div>
         </div>
-    </div>   
- 
+    </div>
         ;
     }
-
 };
---
+++ RenewReact/App/Jsx/Common/Panels3.js
@@ -0,0 +1,89 @@
+﻿import React, { Component } from 'react';
+import style from '../../css/site.css';
+export default class Panels3 extends Component {
+    displayName: 'Panels3';
+    constructor(props) {
+        super(props);
+    };
+    render() {
+        return  <div className="panel panel-default">
+<div className="panel-heading">
+<h3 className="panel-title" id="title">Uppdatera kontoinformation</h3>
+</div>
+<div className="panel-body">
+<form id="update-profile-form">
+<div className="form-group">
+<label className="control-label col-md-3" style={{marginLeft:"-15px",marginRight:"15px"}} htmlFor="firstname">{this.props.text1}</label>
+<div className="col-md-9 input-group">
+<span className="input-group-addon">
+<span className={this.props.icon1}></span>
+</span>
+<input className="form-control"
+data-autoajax="true"
+data-val="true"
+data-val-length-min="2"
+data-val-length-max="45"
+data-val-length="Förnamn måste vara mellan 2 och 45 tecken"
+data-val-required="Förnamn får inte vara tom"
+id="firstname"
+name="firstname"
+type="text"
+value="" />
+</div>
+</div>
+<div className="form-group">
+<label className="control-label col-md-3" style={{marginLeft:"-15px",marginRight:"15px"}} htmlFor="lastname">{this.props.text2}</label>
+<div className="col-md-9 input-group">
+<span className="input-group-addon">
+<span className={this.props.icon2}></span>
+</span>
+<input className="form-control"
+data-autoajax="true"
+data-val="true"
+data-val-length-min="2"
+data-val-length-max="45"
+data-val-length="Efternamn måste vara mellan 2 och 45 tecken"
+data-val-required="Efternamn får inte vara tom"
+id="lastname"
+name="lastname"
+type="text"
+value="" />
+</div>
+</div>
+<div className="form-group">
+<label className="control-label col-md-3" style={{marginLeft:"-15px",marginRight:"15px"}} htmlFor="phone">{this.props.text3}</label>
+<div className="col-md-9 input-group">
+<span className="input-group-addon">
+<span className={this.props.icon3}></span>
+</span>
+<input className="form-control"
+data-autoajax="true"
+data-val="true"
+data-val-length-max="45"
+data-val-length="Telefon får inte överstiga 45 tecken"
+id="phone"
+name="phone"
+//onKeyPress = "return Site.Validation.isPhoneNumber(event)"
+type="text"
+value="" />
+</div>
+</div>
+<div className="row">
+<div className="col-md-6">
+</div>
+<div className="col-md-6">
+<button className="site-buttonmargin-left pull-right btn-info btn update-profile-form-buttons" data-action="8" type="button">Spara</button>
+</div>
+</div>
+</form></div></div>                
+         ;
+    }
+};
+Panels3.propTypes= {
+    text1: React.PropTypes.string.isRequired,
+    text2: React.PropTypes.string.isRequired,
+    text3: React.PropTypes.string.isRequired,
+    icon1: React.PropTypes.string.isRequired,
+    icon2: React.PropTypes.string.isRequired,
+    icon3: React.PropTypes.string.isRequired,
+};
